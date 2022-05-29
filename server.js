9e7ee9a0a954e3306df815e4770f88b6e7e7c6a4
kanwal <kanwal-usman@hotmail.com>
2022-05-30 00:40:22 +0500

    genesis

---
+++ server.js
@@ -0,0 +1,77 @@
+// =====================================EXPRESS SET UP=================================================
+const express = require('express');
+const { retry } = require('rxjs');
+const app = express();
+// set our port
+const port = 8086;
+
+var cors = require('cors')
+app.use(cors());
+
+//=========================================mongodb set up ===========================================
+// Connect using a MongoClient instance
+const MongoClient = require("mongodb").MongoClient;
+
+// Connection url
+const url = "mongodb://localhost:27017";
+// Database Name
+const dbName = "mydatabases";
+
+// Connect using MongoClient
+const mongoClient = new MongoClient(url);
+//===========================================================
+
+//==================================API CALLS======================================================
+app.get('/', (req, res) => res.send('Server has started!'));
+
+app.get('/getVehicles', function(req, res){
+  mongoClient.connect(function (err, client) {
+      console.log("MongoDB Connected")
+      const db = client.db(dbName);
+  db.collection("cars")
+      .find()
+      .toArray(function (err, rows) {
+      if (err) throw err;
+      res.send(rows);
+  });
+  })
+})
+
+app.delete('/delVehicle', function (req, res) {
+  mongoClient.connect(function (err, client) {
+    console.log("MongoDB trying to delete")
+    const db = client.db(dbName);
+    if (err){
+        console.log("Error deleting :((")
+    }else{
+        db.collection("cars").deleteOne({"title": req.query.name});
+    }
+    res.send('Deletion has been completed');
+  })
+})
+
+app.post('/AddVehicles', function(req, res){
+  console.log("Got a POST request for /Add --> name="+req.query.title+" manf="+req.query.manf + " loc="+req.query.loc);
+  mongoClient.connect(function (err, client) {
+      console.log("MongoDB Connected")
+      const db = client.db(dbName);
+      db.collection("cars").insertOne({
+      title: req.query.title,
+      manufacturer: req.query.manf,
+      location: req.query.loc,
+      price: req.query.price,
+      rating:req.query.rating,
+      description:req.query.desc,
+      features:req.query.ft,
+      contact:req.query.contact,
+      email:req.query.email,
+      date_added:req.query.dt,
+      seller_comments:req.query.slr,
+      img: req.query.title+".jpg"
+    });
+  res.send("Received data:: title ="+req.query.title);
+})
+})
+
+// startup our app at http://localhost:3000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [getMessage,setGetMessage] = useState({})
+
+  useEffect(()=>{
+    axios.get('http://localhost:8086/getVehicles').then(response => {
+      console.log("SUCCESS", response)
+      setGetMessage(response)
+    }).catch(error => {
+      console.log(error)
+    })
+  
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {getMessage.status === 200 ?<p>
+        {getMessage.data.map((item, i) => { return<div>
+          <table style={{width:"100%"}}>
+            <tr>
+              <th>{item.title}</th>
+              <th style = {{textAlign:"right", padding:"10px"}}><right>Rs.{item.price}</right></th>
+            </tr>
+            <tr>
+              <th>Qnty: {item.location}</th>
+              <th style = {{textAlign:"right"}}><button>BUTTON</button></th>
+            </tr>
+          </table>
+        </div>
+        })}      
+        </p>:
+           <p>NO ITEM</p>
+        } 
     </div>
   );
 }
